7302129b3318bf58df3c7cfbe2aa5775306bf44f
Yobato <satriyobagus30077@gmail.com>
2023-07-04 21:32:46 +0700

    CRUD-Mitra Done

---
+++ app/Models/Mitra.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mitra extends Model
+{
+    use HasFactory;
+    protected $table = "mitra";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+    protected $fillable = [
+        'nama_mitra', 'role'
+    ];
+}
