bad1772bf3b19a47d6732852bd6e34951a97879d
Rahanug <rahanug@gmail.com>
2023-07-31 22:50:51 +0700

    coba

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
     {
         return view('admin.dashboard.index', [
             "title" => "Dashboard",
+            "tai"=> 'tai'
         ]);
     }
 }
